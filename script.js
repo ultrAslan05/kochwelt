a98aaff4ab85d1c4e98bf919c7669b9ec1157ca2
ultrAslan05 <codearcher204@gmail.com>
2023-12-22 17:03:34 +0100

    calculator function is working

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
 let recipes = {
     test: {
-        ingredient: ['test',],
-        amount: ['890'],
+        ingredient: ["test"],
+        amount: ["890"],
     },
     lahmacunIngredients: {
-        ingredient: ['Mehl', 'Trockenhefe', 'Olivenöl', 'Zucker', 'Salz', 'Wasser', 'Hackfleisch', 'Zwiebel', 'Tomatenmark', 'Petersilie', 'Knoblauch', 'Tomaten', 'Pfeffer'],
-        amount: ['500', '1', '1', '1', '1', '1', '500', '2', '2', '1', '4', '2', '1',],
-        unit: ['g', ' Packung', ' EL', ' TL', ' TL', ' Tasse', 'g', ' ganze', ' EL', ' Bund', ' Zehe/n', '', ' TL']
+        ingredient: ["Mehl", "Trockenhefe", "Olivenöl", "Zucker", "Salz", "Wasser", "Hackfleisch", "Zwiebel", "Tomatenmark", "Petersilie", "Knoblauch", "Tomaten", "Pfeffer"],
+        amount: ["125", "0.25", "0.25", "0.25", "0.25", "0.25", "125", "0.5", "0.25", "0.5", "1", "0.5", "0.25"],
+        unit: ["g", " Packung", " EL", " TL", " TL", " Tasse", "g", " ganze", " EL", " Bund", " Zehe/n", "", " TL"],
     },
 };
 
 function calculator(recipe) {
     let ingredientData = recipes[recipe];
-    let portion = document.getElementById('ingredient-table');
-    let portionSize = document.getElementById('amount').value;
-    portion.innerHTML = '';
+    let portion = document.getElementById("ingredient-table");
+    let portionSize = document.getElementById("amount").value;
+    portion.innerHTML = "";
 
-    for (let i = 0; i < ingredientData['ingredient'].length; i++) {
-        const ingredient = ingredientData['ingredient'][i];
-        let amount = ingredientData['amount'][i];
-        let unit = ingredientData['unit'][i];
-        
+    for (let i = 0; i < ingredientData["ingredient"].length; i++) {
+        const ingredient = ingredientData["ingredient"][i];
+        let amount = ingredientData["amount"][i];
+        let unit = ingredientData["unit"][i];
 
         newAmount = amount * portionSize;
-        console.log(newAmount)
+
         portion.innerHTML += `
             <tbody>
                 <tr>
-                    <td>${amount}${unit} ${ingredient}</td>
+                    <td>${newAmount}${unit} ${ingredient}</td>
                 </tr>
             </tbody>
-        `
+        `;
     }
+    console.log(newAmount);
 }
-
 
 // include template
 async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
+    let includeElements = document.querySelectorAll("[w3-include-html]");
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
         file = element.getAttribute("w3-include-html"); // "includes/header.html"
@@ -45,7 +44,7 @@ async function includeHTML() {
         if (resp.ok) {
             element.innerHTML = await resp.text();
         } else {
-            element.innerHTML = 'Page not found';
+            element.innerHTML = "Page not found";
         }
     }
 }
